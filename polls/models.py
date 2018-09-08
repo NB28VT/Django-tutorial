b82b975c33b7b8a43f144816626ac5666761c7a1
Nathan Burgess <nathanburgess@Nathans-MacBook-Pro-2.local>
2018-09-07 21:44:03 -0400

    Add admin and methods for working with models

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -10,6 +11,12 @@ class Question(models.Model):
     # "date published" argument here is human readable name (optional). Instead of "pub_date"
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     # These are field classes:
     # var is column name, will be used in app but also column name in db
@@ -17,3 +24,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
